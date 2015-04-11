a9edb6c7e61a8b5a34af3f3ccb1a9e9d60a5bfdf
Andressa Andriao <andressaandriao@gmail.com>
2015-04-11 17:42:41 -0300

    Control signals updated

--- src/mascara.h
+++ src/mascara.h
@@ -35,8 +35,8 @@ int S_C
 
 // mascaras
 // usadas para ativar e desativar o bit zero. Atuam na variavel global zero (um char - 8bits)
-#define ativa_bit_zero        0x01	// 00000001		usada com |
-#define desativa_bit_zero     0xfe  // 1111 1110		usada com &
+#define ativa_bit_zero        	0x01	// 00000001		usada com |
+#define desativa_bit_zero     	0xfe  // 1111 1110		usada com &
 
 // usada para separar o bit zero da variavel global zero (um char - 8bits) (posicao 0)
 #define separa_bit_zero			0x01  // 0000 0001		usada com &
@@ -46,22 +46,22 @@ int S_C
 
 // usadas para ativar e zerar a operacao da ULA. Atuam em SC_ULA 3-0
 #define ativa_soma				0x02	// 00000010		usada com |
-#define ativa_subtracao				0x06	// 00000110		usada com |
+#define ativa_subtracao			0x06	// 00000110		usada com |
 #define ativa_and				0x00	// 00000000		usada com |
 #define ativa_or				0x01	// 00000001		usada com |
 #define ativa_slt				0x07	// 00000111		usada com |
 #define ativa_nor				0x0c	// 00001100		usada com |
-#define zera_controle_ULA			0xf0  	// 11110000		usada com &
+#define zera_controle_ULA		0xf0  	// 11110000		usada com &
 
 // mascaras usadas para separar os campos dentro da instrucao
 //						 			       31  26 25 21 20 16 15 11 10  6 5    0
-#define separa_cop			0xfc000000	// 111111 00000 00000 00000 00000 000000  usada com &
-#define separa_rs			0x03e00000	// 000000 11111 00000 00000 00000 000000  usada com &
-#define separa_rt			0x001f0000	// 000000 00000 11111 00000 00000 000000  usada com &
-#define separa_rd           		0x0000f800	// 000000 00000 00000 11111 00000 000000  usada com &
+#define separa_cop				0xfc000000	// 111111 00000 00000 00000 00000 000000  usada com &
+#define separa_rs				0x03e00000	// 000000 11111 00000 00000 00000 000000  usada com &
+#define separa_rt				0x001f0000	// 000000 00000 11111 00000 00000 000000  usada com &
+#define separa_rd           	0x0000f800	// 000000 00000 00000 11111 00000 000000  usada com &
 #define separa_cfuncao			0x0000003f	// 000000 00000 00000 00000 00000 111111  usada com &
-#define separa_imediato     		0x0000ffff	// 000000 00000 00000   1111111111111111  usada com &
-#define separa_endereco_jump		0x03ffffff 	// 000000     11111111111111111111111111  usada com &
+#define separa_imediato     	0x0000ffff	// 000000 00000 00000   1111111111111111  usada com &
+#define separa_endereco_jump	0x03ffffff 	// 000000     11111111111111111111111111  usada com &
 
 // usada para eliminar os 2 bits mais significativos do Campo Funcao.
 // Atua na variavel local dentro de UC_ULA
@@ -73,16 +73,16 @@ int S_C
 // usadas para ativar os sinais de controle. Atuam em SC.
 #define ativa_RegDst			0x0001	    // 0000 0000 0000 0001  usada com |
 #define ativa_RegWrite			0x0002      // 0000 0000 0000 0010  usada com |
-#define ativa_ALUSrcA         		0x0004      // 0000 0000 0000 0100  usada com |
-#define ativa_ALUSrcB0        		0x0008      // 0000 0000 0000 1000  usada com |
-#define ativa_ALUSrcB1        		0x0010      // 0000 0000 0001 0000  usada com |
-#define ativa_ALUOp0          		0x0020      // 0000 0000 0010 0000  usada com |
-#define ativa_ALUOp1          		0x0040      // 0000 0000 0100 0000  usada com |
+#define ativa_ALUSrcA         	0x0004      // 0000 0000 0000 0100  usada com |
+#define ativa_ALUSrcB0        	0x0008      // 0000 0000 0000 1000  usada com |
+#define ativa_ALUSrcB1        	0x0010      // 0000 0000 0001 0000  usada com |
+#define ativa_ALUOp0       		0x0020      // 0000 0000 0010 0000  usada com |
+#define ativa_ALUOp1       		0x0040      // 0000 0000 0100 0000  usada com |
 #define ativa_PCSource0   		0x0080      // 0000 0000 1000 0000  usada com |
 #define ativa_PCSource1	 		0x0100      // 0000 0001 0000 0000  usada com |
 #define ativa_PCWriteCond		0x0200      // 0000 0010 0000 0000  usada com |
 #define ativa_PCWrite			0x0400      // 0000 0100 0000 0000  usada com |
-#define ativa_IorD			0x0800      // 0000 1000 0000 0000  usada com |
+#define ativa_IorD				0x0800      // 0000 1000 0000 0000  usada com |
 #define ativa_MemRead			0x1000      // 0001 0000 0000 0000  usada com |
 #define ativa_MemWrite			0x2000      // 0010 0000 0000 0000  usada com |
 #define ativa_MemtoReg			0x4000      // 0100 0000 0000 0000  usada com |
@@ -98,7 +98,7 @@ int S_C
 #define desativa_ALUOp1				0xffbf      // 1111 1111 1011 1111  usada com &
 #define desativa_PCSource0			0xff7f      // 1111 1111 0111 1111  usada com &
 #define desativa_PCSource1			0xfeff      // 1111 1110 1111 1111  usada com &
-#define desativa_PCWriteCond			0xfdff      // 1111 1101 1111 1111  usada com &
+#define desativa_PCWriteCond		0xfdff      // 1111 1101 1111 1111  usada com &
 #define desativa_PCWrite			0xfbff      // 1111 1011 1111 1111  usada com &
 #define desativa_IorD				0xf7ff      // 1111 0111 1111 1111  usada com &
 #define desativa_MemRead			0xefff      // 1110 1111 1111 1111  usada com &
@@ -107,8 +107,8 @@ int S_C
 #define desativa_IRWrite			0x7fff      // 0111 1111 1111 1111  usada com &
 
 // usadas para separar os sinais de controle. Atuam em SC.
-#define separa_RegDst	 	0x0001	    // 0000 0000 0000 0001  usada com |
-#define separa_RegWrite	   	0x0002      // 0000 0000 0000 0010  usada com |
+#define separa_RegDst	 		0x0001	    // 0000 0000 0000 0001  usada com |
+#define separa_RegWrite	   		0x0002      // 0000 0000 0000 0010  usada com |
 #define separa_ALUSrcA       	0x0004      // 0000 0000 0000 0100  usada com |
 #define separa_ALUSrcB0        	0x0008      // 0000 0000 0000 1000  usada com |
 #define separa_ALUSrcB1        	0x0010      // 0000 0000 0001 0000  usada com |
